3707e9bdb21177a25fa52988d5a6b17dc34593c1
Coverling <goracevd2005@gmail.com>
2024-12-17 14:03:59 +0300

    hello

--- projects/1_hello/main.go
+++ projects/1_hello/main.go
@@ -1,7 +1,28 @@
 package main
 
-// здесь надо написать код
+// некоторые импорты нужны для проверки
+import (
+	"fmt"
+	//"io"
+	"net/http"
+	//"os"
+	//"time"
+)
+
+// Обработчик HTTP-запросов
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Привет, web!"))
+}
 
 func main() {
-	// и здесь тоже
+	// здесь ваш код
+
+	// Регистрируем обработчик для пути "/"
+	http.HandleFunc("/get", handler)
+
+	// Запускаем веб-сервер на порту 8080
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Ошибка запуска сервера:", err)
+	}
 }
--- projects/3_count/main.go
+++ projects/3_count/main.go
@@ -2,9 +2,37 @@ package main
 
 import (
 	"fmt"
-	// "strconv" // вдруг понадобится ;)
+	"log"
+	"net/http"
+	"strconv"
 )
 
+var counter = 0
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		a, err := strconv.Atoi(r.FormValue("count"))
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("это не число"))
+			return
+		}
+		counter += a
+		message := "Число было добавлено!"
+		w.Write([]byte(message))
+		return
+	} else if r.Method == "GET" {
+		w.Write([]byte(strconv.Itoa(counter)))
+		return
+	}
+	w.Write([]byte("Разрешен только метод POST и GET!"))
+}
+
 func main() {
-	fmt.Println("Hi, Bro!!! 🥴")
+	http.HandleFunc("/count", handler)
+	err := http.ListenAndServe(":3333", nil)
+	if err != nil {
+		fmt.Println("Ошибка запуска сервера:", err)
+	}
 }
